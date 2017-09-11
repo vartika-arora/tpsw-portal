1f1086c08cc196385c4fd5d5dfe3cfedb4cd63e6
Arnav <arnav.kr.grv@gmail.com>
2017-09-11 18:49:52 +0530

    fixed bug in user profile and flash messages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,11 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       fields = current_user.attributes.keys
       fields -= %w{_id encrypted_password sign_in_count roles_mask reset_password_token
-             reset_password_sent_at remember_created_at sign_in_count u_id placed_at
+             reset_password_sent_at remember_created_at sign_in_count u_id placed_at c_at
              current_sign_in_at last_sign_in_at current_sign_in_ip last_sign_in_ip}
       fields.each do |field|
         if current_user[field].blank?
+          flash[:notice] = "Fill this form to proceed"
           redirect_to force_update_url
           break
         end
